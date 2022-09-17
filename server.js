e138af104e98e8f33c7e7ed59b5c0653e14fdfa3
Kofi Junior Eshun <eshunkofi23@gmail.com>
2022-09-17 15:19:12 -0600

    Sixth Commit

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const port = 3000
 app.get('/', require('./routes'))
 
 app.listen(port, ()=>{
-    console.log('Example app listening on port ${port}')
+    console.log('Example app listening on port ' + port)
 })
